0f30dfeaab9010c6a7a0ee1ba464f28996b520cd
Aman Kumar <aman.it360@gmail.com>
2025-03-30 08:18:53 +0530

    Time: 0 ms (100%), Space: 11.9 MB (52.48%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(),strs.end());
-        string ans = "";
-        for(int i =0;i<strs[0].size();i++){
-            if(strs[0][i] != strs[strs.size() -1][i]){
-                return ans;
-            }else{
-                ans.push_back(strs[0][i]);
-            }
-        }
+       
+    if (strs.size() == 1)
+        return strs[0]; // Agar sirf ek string hai, wahi prefix hoga
 
-        return ans;
+    // Sorting se first aur last string lexicographically sabse alag hongi
+    sort(strs.begin(), strs.end());
+
+    string common = ""; // Common prefix store karne ke liye
+    int n = strs.size();
+    string first = strs[0], last = strs[n - 1]; // Pehli aur aakhri string le rahe hain
+
+    // Dono strings ka common prefix find karna hai
+    for (int i = 0; i < min(first.size(), last.size()); i++)
+    {
+        if (first[i] != last[i])
+        { // Jaise hi mismatch mile, break kar do
+            return common;
+        }
+        common += first[i]; // Common character ko add karo
+    }
+
+    return common; // Final common prefix return karo
     }
 };
