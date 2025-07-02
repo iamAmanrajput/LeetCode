b86455ba96aa2d74ef8ea2ce66778aaa6305b0cc
Aman Kumar <aman.it360@gmail.com>
2025-07-02 11:14:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+void findCombination(vector<int>& candidates, int target,
+                         vector<vector<int>>& ans, vector<int> &currAns,int index) {
+        if (target == 0) {
+            ans.push_back(currAns);
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = index; i < candidates.size(); i++) {
+            currAns.push_back(candidates[i]);
+            findCombination(candidates, target - candidates[i], ans, currAns,i+1);
+            currAns.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+          vector<vector<int>> ans;
+        vector<int> currAns;
+        findCombination(candidates, target, ans, currAns,0);
+        set<vector<int>>st;
+        for(auto e: ans){
+            st.insert(e);
+        }
+        ans.clear();
+        for(auto e:st){
+            ans.push_back(e);
+        }
+
+        return ans;
+    }
+};
