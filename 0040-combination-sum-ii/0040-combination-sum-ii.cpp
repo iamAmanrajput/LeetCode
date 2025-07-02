1230af37bba366c13ca70eae3aadfe18c86a44b9
Aman Kumar <aman.it360@gmail.com>
2025-07-02 11:14:51 +0530

    Time: 7 ms (13.57%), Space: 14.5 MB (22.56%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-void findCombination(vector<int>& candidates, int target,
-                         vector<vector<int>>& ans, vector<int> &currAns,int index) {
+    void findCombination(vector<int>& candidates, int target,
+                         vector<vector<int>>& ans, vector<int>& currAns,
+                         int index) {
         if (target == 0) {
             ans.push_back(currAns);
             return;
@@ -11,24 +12,28 @@ void findCombination(vector<int>& candidates, int target,
         }
 
         for (int i = index; i < candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
             currAns.push_back(candidates[i]);
-            findCombination(candidates, target - candidates[i], ans, currAns,i+1);
+            findCombination(candidates, target - candidates[i], ans, currAns,
+                            i + 1);
             currAns.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-          vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
         vector<int> currAns;
-        findCombination(candidates, target, ans, currAns,0);
-        set<vector<int>>st;
-        for(auto e: ans){
+        findCombination(candidates, target, ans, currAns, 0);
+        set<vector<int>> st;
+        for (auto e : ans) {
             st.insert(e);
         }
         ans.clear();
-        for(auto e:st){
-            ans.push_back(e);
+        for (auto e : st) {
+            ans.push_back(e); // only unique vectors
         }
 
         return ans;
