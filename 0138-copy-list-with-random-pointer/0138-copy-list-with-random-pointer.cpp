7f74eb708ac493f5dead062ee0dfd9aa7697f75a
Aman Kumar <aman.it360@gmail.com>
2025-08-16 11:32:37 +0530

    Time: 2 ms (92.64%), Space: 14.8 MB (95.61%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,21 +16,40 @@ public:
 
 class Solution {
 public:
-    Node* helper(Node* head, unordered_map<Node*, Node*>& mp) {
-        if (head == 0)
+    Node* copyRandomList(Node* head) {
+        if (!head)
             return 0;
 
-        Node* newHead = new Node(head->val);
-        mp[head] = newHead;
-        newHead->next = helper(head->next, mp);
-        if (head->random) {
-            newHead->random = mp[head->random];
+        // step 1 : Clone A -> A'
+        Node* it = head; // iterate over old head
+        while (it) {
+            Node* clonedNode = new Node(it->val);
+            clonedNode->next = it->next;
+            it->next = clonedNode;
+            it = it->next->next;
         }
-        return newHead;
-    }
 
-    Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp; // old node --> new node
-        return helper(head, mp);
+        // step 2 : Assign random links of A' with the help of A
+        it = head;
+        while (it) {
+            Node* clonedNode = it->next;
+            clonedNode->random = it->random ? it->random->next : nullptr;
+            it = it->next->next;
+        }
+
+        // step 3 : Detach A
+        it = head;
+        Node* clonedHead = it->next;
+
+        while (it) {
+            Node* clonedNode = it->next;
+            it->next = it->next->next;
+            if (clonedNode->next) {
+                clonedNode->next = clonedNode->next->next;
+            }
+            it = it->next;
+        }
+
+        return clonedHead; 
     }
 };
