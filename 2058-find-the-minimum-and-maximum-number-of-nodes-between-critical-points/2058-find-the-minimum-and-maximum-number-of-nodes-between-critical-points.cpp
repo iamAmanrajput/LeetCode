9fabb801bd605907dced8df73693ba9a98d61a27
Aman Kumar <aman.it360@gmail.com>
2025-08-17 20:57:10 +0530

    Time: 0 ms (100%), Space: 124.7 MB (60.82%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> ans = {-1, -1}; // minDis , maxDis
+        ListNode* prev = head;
+        if (!prev) {
+            return ans;
+        }
+        ListNode* curr = head->next; // bcz its cumpulsory to have prev node
+        if (!curr) {
+            return ans;
+        }
+        ListNode* nxt = head->next->next;
+        if (!nxt) {
+            return ans;
+        }
+
+        int firstCP = -1;
+        int lastCP = -1;
+        int minDist = INT_MAX;
+        int i = 1;
+        while (nxt) {
+            bool isCP =
+                ((curr->val > prev->val &&
+                  curr->val > nxt->val) ||                       // Local maxima
+                 (curr->val < prev->val && curr->val < nxt->val) // Local minima
+                );
+
+            if (isCP && firstCP == -1) {
+                firstCP = i;
+                lastCP = i;
+            } else if (isCP) {
+                minDist = min(minDist, i - lastCP);
+                lastCP = i;
+            }
+            i++;
+            prev = prev->next;
+            curr = curr->next;
+            nxt = nxt->next;
+        }
+        if (lastCP == firstCP) {
+            // only 1 CP found
+            return ans;
+        } else {
+            ans[0] = minDist;
+            ans[1] = lastCP - firstCP;
+        }
+        return ans;
+    }
+};
