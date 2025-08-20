ad686649962b89ce35da55ee6141b55989d9f625
Aman Kumar <aman.it360@gmail.com>
2025-08-20 17:37:48 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-    int n = nums.size();
-    int total = n * (n + 1) / 2;
-    int sum = 0;
-    for (int num : nums) {
-        sum += num;
-    }
-    return total - sum;
-}
+        // n is the size of the array
+        int n = nums.size();
 
+        // Calculate the expected sum of numbers from 0 to n using the formula:
+        // n*(n+1)/2
+        int total = n * (n + 1) / 2;
+
+        // Calculate the sum of all elements present in the array
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        // The missing number is the difference between the expected total sum
+        // and the actual sum of elements
+        return total - sum;
+    }
 };
