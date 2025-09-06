ff46f40638c3d25b5609db0c3ef120f519c4043d
Aman Kumar <aman.it360@gmail.com>
2025-09-06 20:51:36 +0530

    Time: 35 ms (88.75%), Space: 54.6 MB (10.79%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,11 +4,16 @@
  * @return {number[]}
  */
 var filter = function (arr, fn) {
-    const ans = [];
-    for (let i = 0; i < arr.length; i++) {
+    let n = arr.length;
+    let ans = new Array(n);  // preallocate array (faster than push)
+    let size = 0;
+
+    for (let i = 0; i < n; i++) {
         if (fn(arr[i], i)) {
-            ans.push(arr[i]);
+            ans[size++] = arr[i]; // insert directly
         }
     }
+
+    ans.length = size; // trim unused slots
     return ans;
 };
