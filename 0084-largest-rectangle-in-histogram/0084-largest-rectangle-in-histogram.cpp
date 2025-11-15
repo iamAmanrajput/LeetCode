2d4b78419508209b9594650b0d5d50cbfaeed6ba
Aman Kumar <aman.it360@gmail.com>
2025-11-15 15:08:36 +0530

    Time: 54 ms (8.63%), Space: 85.6 MB (45.38%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<int> prevSmaller(vector<int>& v) {
+        stack<int> st;
+        st.push(-1);
+
+        vector<int> ans(v.size());
+
+        for (int i = 0; i < v.size(); i++) {
+            int curr = v[i];
+            while (st.top() != -1 && v[st.top()] >= curr) {
+                st.pop();
+            }
+            ans[i] = st.top();
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    vector<int> nextSmaller(vector<int>& v) {
+        vector<int> ans(v.size());
+        stack<int> s;
+        s.push(-1);
+
+        for (int i = v.size() - 1; i >= 0; i--) {
+
+            int curr = v[i];
+
+            // apka answer stack me
+            while (s.top() != -1 && v[s.top()] >= curr) {
+                s.pop();
+            }
+
+            // chotta element mil chuka h -> ans store
+            ans[i] = s.top();
+
+            // push krdo curr element ko
+            s.push(i);
+        }
+
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> prev = prevSmaller(heights);
+        vector<int> next = nextSmaller(heights);
+
+        int maxArea = INT_MIN;
+
+        for(int i =0;i<next.size();i++){
+            if(next[i]== -1){
+                next[i] = next.size();
+            }
+        }
+
+        for(int i =0;i<heights.size();i++){
+            int length = heights[i];
+            int width = next[i] - prev[i] -1;
+            int area = length * width;
+            maxArea = max(maxArea,area);
+        }
+
+        return maxArea;
+    }
+};
