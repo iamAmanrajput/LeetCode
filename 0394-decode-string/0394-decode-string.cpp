7046923adf2529b2fc8594b1247be1ce74049bbf
Aman Kumar <aman.it360@gmail.com>
2025-12-14 13:52:08 +0530

    Time: 3 ms (11.24%), Space: 9.4 MB (47.19%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        for (auto ch : s) {
+            if (ch == ']') {
+                string stringToRepeat = "";
+                while (!st.empty() && !isdigit(st.top()[0])) {
+                    string top = st.top();
+                    stringToRepeat += top == "[" ? "" : top;
+                    st.pop();
+                }
+                string numericTimes = "";
+                while (!st.empty() && isdigit(st.top()[0])) {
+                    numericTimes += st.top();
+                    st.pop();
+                }
+                reverse(numericTimes.begin(), numericTimes.end());
+                int n = stoi(numericTimes);
+                string currentDecode = "";
+                while (n--) {
+                    currentDecode += stringToRepeat;
+                }
+                st.push(currentDecode);
+            } else {
+                string temp(1, ch);
+                st.push(temp);
+            }
+        }
+
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
