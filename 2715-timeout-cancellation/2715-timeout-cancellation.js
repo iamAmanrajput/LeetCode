8be73ea4f226d381aec4233b1eda3e526cfd16c9
Aman Kumar <aman.it360@gmail.com>
2025-12-18 19:46:22 +0530

    Time: 55 ms (70.99%), Space: 53.4 MB (97.51%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,42 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  // timeout ko store kar liya
+  const timeoutId = setTimeout(() => {
+    fn(...args); // fn ko args ke sath call kiya
+  }, t);
+
+  // cancel function return kar rahe hain
+  return function cancelFn() {
+    clearTimeout(timeoutId); // timeout cancel
+  };
+};
+
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
