bb7443cb13458e5668dc8b8d33971d15bf3c5474
Aman Kumar <aman.it360@gmail.com>
2026-04-15 11:00:49 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solveUsingTabulation(int n) {
-        
+
         // Step 1: Create a DP array of size (n + 1)
         // This array will store Fibonacci values from 0 to n
         vector<int> dp(n + 1, -1);
@@ -10,6 +10,9 @@ public:
         // We already know:
         // fib(0) = 0 and fib(1) = 1
         dp[0] = 0;
+        if (n == 0) {
+            return dp[0];
+        }
         dp[1] = 1;
 
         // Step 3: Fill the DP array from index 2 to n
