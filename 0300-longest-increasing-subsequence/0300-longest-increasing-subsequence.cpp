db801969ab02384fd3ab4aa007bd1e5064d0688c
Aman Kumar <aman.it360@gmail.com>
2026-04-23 11:08:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int curr, int prev, vector<vector<int>>& dp) {
-        if (curr >= nums.size()) {
-            return 0;
-        };
-
-        if (dp[curr][prev + 1] != -1) {
-            return dp[curr][prev + 1];
-        }
-
-        int include = 0;
-        if (prev == -1 || nums[prev] < nums[curr]) {
-            include = 1 + solve(nums, curr + 1, curr, dp);
-        }
-        int exclude = 0 + solve(nums, curr + 1, prev, dp);
-
-        dp[curr][prev + 1] = max(include, exclude);
-        return dp[curr][prev + 1];
-    };
-
     int lengthOfLIS(vector<int>& nums) {
-        int curr = 0;
-        int prev = -1;
         vector<vector<int>> dp(nums.size() + 1,
-                               vector<int>(nums.size() + 1, -1));
-        int ans = solve(nums, curr, prev, dp);
-        return ans;
+                               vector<int>(nums.size() + 1, 0));
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= -1; j--) {
+
+                int include = 0;
+                if (j == -1 || nums[j] < nums[i]) {
+                    include = 1 + dp[i + 1][i + 1];
+                }
+                int exclude = 0 + dp[i + 1][j + 1];
+
+                dp[i][j + 1] = max(include, exclude);
+            }
+        }
+
+        return dp[0][0];
     }
 };
