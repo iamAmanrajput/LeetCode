0d0cb4e4c3edb21684d03fbb65c867625594d6e8
Aman Kumar <aman.it360@gmail.com>
2026-04-25 10:50:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     long long int MOD = 1000000007;
 
-    int numRollsToTarget(int& n, int& k, int& target) {
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
-        dp[0][0] = 1;
+    int numRollsToTarget(int n, int k, int target) {
+        vector<int> prev(target + 1, 0);
+        vector<int> curr(target + 1, 0);
+        prev[0] = 1;
 
         for (int i = 1; i <= n; i++) {
+            // Reset curr for the current die so old values don't interfere
+            fill(curr.begin(), curr.end(), 0);
             for (int j = 1; j <= target; j++) {
                 int ans = 0;
                 for (int idx = 1; idx <= k; idx++) {
                     if (j - idx >= 0) {
-                        ans = (ans + dp[i - 1][j - idx]) % MOD;
+                        ans = (ans + prev[j - idx]) % MOD;
                     };
                 }
-                dp[i][j] = ans;
+                curr[j] = ans;
             }
+            prev = curr;
         }
 
-        return dp[n][target];
+        return prev[target];
     }
 };
