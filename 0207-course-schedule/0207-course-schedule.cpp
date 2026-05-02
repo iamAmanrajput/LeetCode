01fe12ddd8c58ac6fa2019cdee6e6c7d61dd3baa
Aman Kumar <aman.it360@gmail.com>
2026-05-02 13:16:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
 
-unordered_map<int,list<int>> adj;
-
     // DFS function to detect cycle
-    bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath) {
+    bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath,
+                    vector<vector<int>>& adj) {
 
         vis[src] = true;
         recPath[src] = true;
@@ -13,7 +12,7 @@ unordered_map<int,list<int>> adj;
         for (auto v : adj[src]) {
 
             if (!vis[v]) {
-                if (isCycleDFS(v, vis, recPath)) {
+                if (isCycleDFS(v, vis, recPath, adj)) {
                     return true;
                 }
             }
@@ -28,10 +27,13 @@ unordered_map<int,list<int>> adj;
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
+        // Step 1: Build adjacency list
+        vector<vector<int>> adj(numCourses);
+
         for (auto &p : prerequisites) {
-            int v = p[0];
-            int u = p[1];
-            adj[u].push_back(v);
+            int a = p[0];
+            int b = p[1];
+            adj[b].push_back(a); // b -> a
         }
 
         vector<bool> vis(numCourses, false);
@@ -40,7 +42,7 @@ unordered_map<int,list<int>> adj;
         // Step 2: Check cycle in every component
         for (int i = 0; i < numCourses; i++) {
             if (!vis[i]) {
-                if (isCycleDFS(i, vis, recPath)) {
+                if (isCycleDFS(i, vis, recPath, adj)) {
                     return false; // cycle hai → finish nahi kar sakte
                 }
             }
