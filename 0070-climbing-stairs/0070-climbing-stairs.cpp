4159f8b47f2441fa232e703d6f4c692af24f1cc2
Aman Kumar <aman.it360@gmail.com>
2026-05-20 10:06:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int> dp(n + 1);
+    int solve(int n, vector<int>& dp) {
 
-        dp[0] = 1;
-        dp[1] = 1;
+        // base case
+        if (n == 0 || n == 1)
+            return 1;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
+        // agar pehle se calculate hai
+        if (dp[n] != -1)
+            return dp[n];
+
+        // recursion + store in dp
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
 
         return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+
+        return solve(n, dp);
     }
 };
