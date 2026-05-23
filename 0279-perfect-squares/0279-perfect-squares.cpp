9908ae99afc5bad545667ee1e0087255e574ea78
Aman Kumar <aman.it360@gmail.com>
2026-05-23 11:16:35 +0530

    Time: 103 ms (23.21%), Space: 14.6 MB (20.41%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& nums, vector<int>& dp) {
-        if (n == 0) {
-            return 0;
-        }
+    int solve(int n, vector<int>& squares, vector<int>& dp) {
 
-        if (dp[n] != -1) {
+        if (n == 0)
+            return 0;
+
+        if (dp[n] != -1)
             return dp[n];
-        }
 
         int mini = INT_MAX;
-        for (int i = 0; i < nums.size(); i++) {
-            if (n - nums[i] * nums[i] >= 0) {
-                int ans = solve(n - nums[i] * nums[i], nums, dp);
-                if (ans != INT_MAX) {
-                    mini = min(mini, ans + 1);
-                }
-            }
+
+        for (int square : squares) {
+
+            if (square > n)
+                break;
+
+            int ans = solve(n - square, squares, dp);
+
+            mini = min(mini, ans + 1);
         }
 
         return dp[n] = mini;
@@ -24,16 +25,13 @@ public:
 
     int numSquares(int n) {
 
-        vector<int> nums;
+        vector<int> squares;
 
-        int i = 1;
-        while (i * i <= n) {
-            nums.push_back(i);
-            i++;
-        }
+        for (int i = 1; i * i <= n; i++)
+            squares.push_back(i * i);
 
         vector<int> dp(n + 1, -1);
 
-        return solve(n, nums, dp);
+        return solve(n, squares, dp);
     }
 };
