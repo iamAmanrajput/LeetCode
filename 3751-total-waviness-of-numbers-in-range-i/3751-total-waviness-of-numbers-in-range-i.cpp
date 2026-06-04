0e97a444b702817213553fe1c19d5d509c68c78a
Aman Kumar <aman.it360@gmail.com>
2026-06-04 22:10:54 +0530

    Time: 30 ms (83.96%), Space: 9.9 MB (46.64%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     bool isWaviness(int prevDig, int currDig, int nextDig) {
-        return (currDig > prevDig && currDig > nextDig) ||
-               (currDig < prevDig && currDig < nextDig);
-    }
+        if (currDig > prevDig && currDig > nextDig) {
+            return true;
+        }
+        if (currDig < prevDig && currDig < nextDig) {
+            return true;
+        }
+        return false;
+    };
 
     int totalWaviness(int num1, int num2) {
+        if (num1 < 100 && num2 < 100) {
+            return 0;
+        };
 
         int waviness = 0;
+        stack<int> st;
+        int cnt = 0;
+        int temp = num2;
 
-        for (int num = num1; num <= num2; num++) {
-
-            string s = to_string(num);
-
-            for (int i = 1; i < s.size() - 1; i++) {
-
-                int prev = s[i - 1] - '0';
-                int curr = s[i] - '0';
-                int next = s[i + 1] - '0';
-
-                if (isWaviness(prev, curr, next)) {
-                    waviness++;
-                }
-            }
+        while (temp) {
+            cnt++;
+            temp /= 10;
         }
 
+        vector<int> arr(cnt, -1);
+
+        for (int i = num1; i <= num2; i++) {
+            int digit = i;
+            while (digit) {
+                int lastDig = digit % 10;
+                st.push(lastDig);
+                digit = digit / 10;
+            }
+            int arrIndex = 0;
+            while (!st.empty()) {
+                int top = st.top();
+                arr[arrIndex] = top;
+                st.pop();
+                arrIndex++;
+            }
+
+            int index = 1;
+            while (index < arr.size() - 1 && arr[index] != -1) {
+
+                if (arr[index + 1] == -1)
+                    break;
+
+                if (isWaviness(arr[index - 1], arr[index], arr[index + 1])) {
+                    waviness++;
+                }
+
+                index++;
+            }
+        }
         return waviness;
     }
 };
