467dbe76822e8b9c819a63a7e9ba4456a1e3a947
Aman Kumar <aman.it360@gmail.com>
2026-06-16 13:37:22 +0530

    Time: 7 ms (35.61%), Space: 39.8 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+        for(int i = 0; i<s.size();i++){
+            if(isalpha(s[i])){
+                result.push_back(s[i]);
+            }else if(s[i] == '*'){
+                if(result.size() >= 1){
+                    result.pop_back();
+                }
+            }else if(s[i] == '#'){
+                result = result + result;
+            }else{
+                reverse(result.begin(),result.end());
+            }
+        }
+
+        return result;
+    }
+};
